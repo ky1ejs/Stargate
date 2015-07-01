8388129291ea63b700aaf3f6a58c80ee1a06633d
Kyle McAlpine <k@kylejm.io>
2015-07-01 11:13:53 +0100

    Xcode project created

---
+++ Stargate/Stargate.h
@@ -0,0 +1,19 @@
+//
+//  Stargate.h
+//  Stargate
+//
+//  Created by Kyle McAlpine on 01/07/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stargate.
+FOUNDATION_EXPORT double StargateVersionNumber;
+
+//! Project version string for Stargate.
+FOUNDATION_EXPORT const unsigned char StargateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stargate/PublicHeader.h>
+
+
